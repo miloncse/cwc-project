d572cf9f297e249e34de4fc165290a8705891d78
Milon Hossain <milonbdx@gmail.com>
2015-01-21 22:15:57 +0600

    added confirm password validation into edit_profile_info.php

--- application/views/edit_profile_info.php
+++ application/views/edit_profile_info.php
@@ -16,13 +16,27 @@
                     <h2>Profile Info</h2>
                     <form action="<?php echo base_url() . "action/update_profile/" . $user_id; ?>" method="post">
                         Name:<input type="text" name="full_name" placeholder="<?php echo $full_name;?>"/>
-                        Password:<input type="password" name="password" placeholder="New Password"/>
-                        Confirm Password:<input type="password" name="password_confirm" placeholder="Confirm New Password"/>
+                        Password:<input type="password" id="password" name="password" placeholder="New Password"/>
+                        Confirm Password:<input type="password" id="password_confirm" name="password_confirm" placeholder="Confirm New Password"/>
+                        <div id="message" style="color:red;"></div>
                         Phone Number: <input type="text" name="phone_number" placeholder="<?php echo $phone_number;?>"/>
                         <button type="submit" class="btn btn-default">Update</button>
                     </form>
                 </div><!--/sign up form-->
             </div>
 </section>
-    
-    
+<script>
+$(document).ready(function(){
+    $("#password_confirm").blur(function(){
+        var  password =$("#password").val();
+        var  password_confirm =$("#password_confirm").val();
+        if(password !=password_confirm){
+            $("#password_confirm").val("");
+            $("#message").html("password does not match")
+        }else{
+          $("#message").html("")  
+        }
+    })     
+})
+
+</script>
